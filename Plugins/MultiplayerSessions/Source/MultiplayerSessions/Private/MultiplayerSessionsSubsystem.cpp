6544db11b860c90c84b3524ac937d7dce94301a2
Sreyash Rajalingam Ananth <rasreyash@gmail.com>
2024-11-28 12:27:08 +0530

    Multiplayer Sessions Plugin
    Multiplayer Sessions plugin created with build and uplugin files settings configured

---
+++ Plugins/MultiplayerSessions/Source/MultiplayerSessions/Private/MultiplayerSessionsSubsystem.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MultiplayerSessionsSubsystem.h"
+#include "OnlineSubsystem.h"
+
+UMultiplayerSessionsSubsystem::UMultiplayerSessionsSubsystem()
+{
+	if (IOnlineSubsystem* OnlineSubsystem = IOnlineSubsystem::Get())
+	{
+		OnlineSessionInterface = OnlineSubsystem->GetSessionInterface();
+	}
+}
